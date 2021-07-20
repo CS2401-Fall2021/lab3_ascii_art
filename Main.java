6d582614581ac9f4aff4fdb11894ef6eaec52692
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-20 13:43:19 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
   
   public static void main(String[] args) {
     // Part 1
-    Image image = new Image("pup.jpeg");
-    System.out.println(image.getBlackWhiteAscii()); 
+    Image image = new Image("small_pup.png");
+    print(image.getBlackWhiteAscii()); 
     // Part 2
     //System.out.println(image.getGreyAscii());
     
@@ -12,5 +12,15 @@ class Main {
     
     // Part 4
     //System.out.println(image.getResizedGreyAscii(100,100));
+  }
+
+
+  private static void print(char[][] a){		
+    for(int i=0; i<a.length; i++){			
+        for(int j=0; j<a[i].length; j++){
+	        System.out.print(a[i][j]+ " ");	
+	      }
+	    System.out.println();
+    }
   }
 }
