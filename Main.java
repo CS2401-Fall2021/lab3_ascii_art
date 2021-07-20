f0fa99477dfbec6421478434f806faaacb4e7118
m-steph <86425576+m-steph@users.noreply.github.com>
2021-07-20 12:27:55 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  
+  public static void main(String[] args) {
+    // Part 1
+    Image image = loadImage("pup.jpeg");
+    System.out.println(image.getBlackWhiteAscii()); 
+    // Part 2
+    //System.out.println(image.getGreyAscii());
+    
+    // Part 3
+    //System.out.println(image.getRotatedGreyAscii());
+    
+    // Part 4
+    //System.out.println(image.getCroppedGreyAscii());
+  }
